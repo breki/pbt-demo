9578462d0d2ef22385bad9c4aeab80c2533872e0
Igor Brejc <igor.brejc@gmail.com>
2021-04-14 07:13:01 +0200

    Added rgb step 4.

---
+++ rgb/step4/tests/tests.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from hypothesis import given
+from hypothesis.strategies import integers
+
+from rgb.step4.rgb import RgbColor
+
+
+class RgbStep4Tests(TestCase):
+    def test_hex_triplet_representation_is_correct(self):
+        color = RgbColor(33, 52, 165)
+        self.assertEqual("#2134a5", color.to_hex_triplet())
+
+    @given(integers(), integers(), integers())
+    def test_hex_triplet_is_7_characters_long(
+            self, r: int, g: int, b: int):
+        color = RgbColor(r, g, b)
+        triplet = color.to_hex_triplet()
+        print(triplet)
+        self.assertEqual(7, len(triplet))
