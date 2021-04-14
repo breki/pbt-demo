3b75115b900d65a535802be0f72f323f84a0add8
Igor Brejc <igor.brejc@gmail.com>
2021-04-14 07:08:22 +0200

    Added rgb step 3.

---
+++ rgb/step3/rgb.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class RgbColor:
+    r: int
+    g: int
+    b: int
+
+    def to_hex_triplet(self) -> str:
+        return f"#{self._hex(self.r)}{self._hex(self.g)}{self._hex(self.b)}"
+
+    @staticmethod
+    def _hex(value: int) -> str:
+        return "{:02x}".format(value)
--- rgb/step3/tests/tests.py
+++ rgb/step3/tests/tests.py
@@ -2,10 +2,10 @@ from unittest import TestCase
 from hypothesis import given
 from hypothesis.strategies import integers
 
-from rgb.step2.rgb import RgbColor
+from rgb.step3.rgb import RgbColor
 
 
-class RgbStep2Tests(TestCase):
+class RgbStep3Tests(TestCase):
     def test_hex_triplet_representation_is_correct(self):
         color = RgbColor(33, 52, 165)
         self.assertEqual("#2134a5", color.to_hex_triplet())
@@ -14,4 +14,6 @@ class RgbStep2Tests(TestCase):
     def test_hex_triplet_is_7_characters_long(
             self, r: int, g: int, b: int):
         color = RgbColor(r, g, b)
-        self.assertEqual(7, len(color.to_hex_triplet()))
+        triplet = color.to_hex_triplet()
+        print(triplet)
+        self.assertEqual(7, len(triplet))
